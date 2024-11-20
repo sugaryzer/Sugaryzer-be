a39207afff081db6183654e9e5c478c914f94c7e
LUTHFI <luthfinaufalwibisono1@gmail.com>
2024-11-21 00:42:19 +0700

    modified:   .env.example modified:   src/config/config.js

--- src/config/config.js
+++ src/config/config.js
@@ -1,7 +1,6 @@
 module.exports = {
   development: {
     url: process.env.DB_URL || 'postgres://username:password@localhost:port/dbname',
-    port: process.env.DB_PORT,
     dialect: 'postgres',
     dialectOptions: {
       ssl: false,
