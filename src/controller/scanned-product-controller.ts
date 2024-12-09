06afa90a43470e554515e2f154b78f0c2488d2a3
LUTHFI <luthfinaufalwibisono1@gmail.com>
2024-12-09 19:25:49 +0700

    Lint

--- src/controller/scanned-product-controller.ts
+++ src/controller/scanned-product-controller.ts
@@ -1,4 +1,4 @@
-import { Request, Response, NextFunction } from "express";
+import { Response, NextFunction } from "express";
 import { ResponseError } from "../error/response-error";
 import { ScannedProductService } from "../service/scanned-product-service";
 import { CreateScannedProductRequest, ScannedProductGetRequest } from "../model/scanned-product-model";
